866ecaeb3dfd3380843bffdd7bdbc93ff5c36dc6
demetrio_bknp <demetrio@bknp.com.br>
2021-03-07 11:26:45 -0300

    Login and register created

--- app/app.py
+++ app/app.py
@@ -1,14 +1,47 @@
 from flask import Flask, render_template, request, url_for, redirect
+import db
 
 app = Flask(__name__)
+user = db.User()
 
 @app.route("/")
 def beginning():
-    return redirect(url_for('main'))
+    print(url_for('login'))
+    print(url_for('register'))
+    return redirect(url_for('login'))
 
-@app.route("/main")
-def main():
-    return render_template('main.html')
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+        if request.form['button'] == 'ENTRAR':
+            name = request.form['name_cliente']
+            email = request.form['email_cliente']
+            try:
+                user.get_user_info(name, email)
+            except Exception as e:
+                print(e)
+                return render_template('login.html', login_fail=True)
+        elif request.form['button'] == 'CADASTRAR':
+            return redirect(url_for('register'))
+    else:
+        return render_template('login.html')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        if request.form['button'] == 'CADASTRAR':
+            name = request.form['name_cliente']
+            email = request.form['email_cliente']
+            try:
+                user.insert_into_clientes(name, email)
+                return render_template('register.html', register_sucess=True)
+            except:
+                return render_template('register.html', register_fail=True)
+        elif request.form['button'] == 'LOGIN':
+            return redirect(url_for('login'))
+    else:
+        return render_template('register.html')
 
 if __name__ == '__main__':
     app.run()
--- app/db.py
+++ app/db.py
@@ -45,21 +45,42 @@ class DB():
             conn.close()
             return True
         except Exception as e:
-            print(e)
-            return False
+            return e
 
     def create_database(self, database_name):
-        query = f"""
-        CREATE DATABASE {database_name}"""
-        print(query)
-        return self.exec_query(query)
+        try:
+            query = f"""
+            CREATE DATABASE {database_name}"""
+            conn = psycopg2.connect(
+                user=self.get_user(),
+                password=self.__get_password(),
+                host=self.get_host(),
+                port=self.get_port(),
+                database='postgres'
+            )
+            conn.autocommit = True
+            cur = conn.cursor()
+            cur.execute(query)
+            cur.close()
+            conn.close()
+        except Exception as e:
+            pass
 
 
 class MagaluDB(DB):
-    def __init__(self, user, password, host='localhost', port=5433, db='magalu'):
+    def __init__(self, user='postgres', password='Menedor0!1', host='localhost', port=5433, db='magalu'):
         super().__init__(user, password, host, port, db)
-        #self.create_database(db)
-        #self.create_clientes_table()
+        self.create_database(db)
+
+    def create_token(self, text):
+        bytes_text = bytes(text.lower(), 'utf-8')
+        token = base64.b64encode(bytes_text).decode()
+        return token
+
+    def check_token(self, token):
+        decoded = base64.b64decode(token).decode()
+        df = pd.read_sql_query('SELECT * from clientes')
+        return decoded in df['token'].values.tolist()
 
     def create_clientes_table(self):
         query = f"""
@@ -87,23 +108,54 @@ class MagaluDB(DB):
     def insert_into_clientes(self, name, email):
         token = self.create_token(name+email)
         query = f"INSERT INTO clientes (cliente_name, cliente_email, cliente_token) VALUES ('{name.lower()}', '{email.lower()}', '{token}')"
-        self.exec_query(query)
-        return query
+        return self.exec_query(query)
 
-    def create_token(self, text):
-        bytes_text = bytes(text.lower(), 'utf-8')
-        token = base64.b64encode(bytes_text).decode()
-        return token
+class User(MagaluDB):
+    def __init__(self):
+        super().__init__()
+        self.__name = None
+        self.__email = None
+        self.__id = None
+        self.__token = None
 
-    def check_token(self, token):
-        decoded = base64.b64decode(token).decode()
-        df = pd.read_sql_query('SELECT * from clientes')
-        return decoded in df['token'].values()
+    def set_name(self, name):
+        self.__name = name
 
-    def get_token(self, name, email):
+    def get_name(self):
+        return self.__name
+
+    def set_email(self, email):
+        self.__email = email
+
+    def get_email(self):
+        return self.__email
+
+    def set_id(self, user_id):
+        self.__id = user_id
+
+    def get_id(self):
+        return self.__id
+
+    def set_token(self, token=None):
+        if not token:
+            token = self.create_token(self.get_name() + self.get_email())
+        self.__token = token
+
+    def get_token(self):
+        return self.__token
+
+    def get_user_info(self, name, email):
         query = f"""
-        SELECT cliente_token FROM clientes
+        SELECT * FROM clientes
         WHERE cliente_name = '{name}' and cliente_email = '{email}'"""
         conn = self.connection()
-        df = pd.read_sql_query(query, conn)
-        return df.iloc[0,0]
+        try:
+            df = pd.read_sql_query(query, conn)
+            user_info = df.iloc[0].values.tolist()
+            conn.close()
+            self.set_id(user_info[0])
+            self.set_name(user_info[1])
+            self.set_email(user_info[2])
+            self.set_token(user_info[3])
+        except:
+            raise Exception("The user and email don't exist")
